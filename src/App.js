1fbbda8263cb6c7ca0a6c910619080d50a5c2340
Paul Korzhyk <paul@dgraph.io>
2019-03-20 17:20:12 +0200

    Add and use todomvc-app-css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
+import 'todomvc-app-css/index.css'
+
 import './App.css'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>TODO: Insert our app here</h1>
-      </div>
+				<header className="header">
+					<h1>todos</h1>
+					<input
+						className="new-todo"
+						placeholder="What needs to be done?"
+						autoFocus={true}
+					/>
+				</header>
+			</div>
     )
   }
 }
